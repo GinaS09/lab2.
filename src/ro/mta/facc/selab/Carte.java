64429b70301c7244b549eb32493a155302d30983
ginas <gina.seres58@gamail.com>
2020-10-23 16:33:31 +0300

    implementare utilizator
    Signed-off-by: ginas <gina.seres58@gamail.com>

--- src/ro/mta/facc/selab/Carte.java
+++ src/ro/mta/facc/selab/Carte.java
@@ -1,12 +1,10 @@
 package ro.mta.facc.selab;
 
-import java.util.Collections;
-
 public class Carte implements Comparable<Carte> {
 
     private String titlu;
     private String autor;
-    private int anAp;
+    private int anPublicatie;
     private int nrPag;
 
 
@@ -23,12 +21,12 @@ public class Carte implements Comparable<Carte> {
     public Carte(String titlu, String autor, int anAp, int nrPag) {
         this.titlu = titlu;
         this.autor = autor;
-        this.anAp = anAp;
+        this.anPublicatie = anAp;
         this.nrPag = nrPag;
     }
 
     @Override
     public String toString() {
-        return this.titlu + " " + this.autor + "Aparuta in anul :" + this.anAp;
+        return this.titlu + " " + this.autor + "Aparuta in anul :" + this.anPublicatie;
     }
 }
---
+++ src/ro/mta/facc/selab/Utilizator.java
@@ -0,0 +1,13 @@
+package ro.mta.facc.selab;
+
+import java.util.ArrayList;
+
+public class Utilizator {
+    private String nume;
+    private ArrayList<Carte> cartiImprumut;
+
+    public Utilizator (String nume){
+        this.nume = nume;
+    }
+
+}
